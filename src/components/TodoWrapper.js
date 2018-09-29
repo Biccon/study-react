8bd9c9b3b443bb92f824b98f6386e3360aeeb673
Biccon <admin@idx.kr>
2018-09-29 15:46:49 +0900

    .

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -16,6 +16,7 @@ class TodoWrapper extends Component {
     console.log("todowrapper : ", this.state.todo);
     return (
       <div className="todo-wrapper">
+        <h2><i class="fas fa-list-ul"></i> What I should to do</h2>
         <TodoItemForm handleAddTodo={this.handleAddTodo} />
         <TodoItemList todo={this.state.todo} />
       </div>
